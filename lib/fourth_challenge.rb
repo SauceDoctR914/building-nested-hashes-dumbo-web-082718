00ab6dc9d2fbf8f5353fe7b433bcc22ac0a1700b
Gavin Soko <sokolofg@gmail.com>
2018-08-21 17:01:29 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{:name => "", :age => "", :attitude => ""}
+      :heroine_friends => [{:name => "Benvolio", :age => "", :attitude => ""}, {:name => "", :age => "", :attitude => ""}, {:name => "", :age => "", :attitude => ""}
+      ]
+      ]
       ]
    }
 }
